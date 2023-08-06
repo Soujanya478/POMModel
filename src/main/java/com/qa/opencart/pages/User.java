a28cfae52489a92a941072166127d28d59181889
Amar Prattipati <75680457+amarprathipati@users.noreply.github.com>
2023-08-06 19:29:39 +0530

    Added user.java file

---
+++ src/main/java/com/qa/opencart/pages/User.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+public class User {
+	
+	public void m1() {
+		int i =10;
+		int j =20;
+	}
+	
+	public void m2() {
+		System.out.println("This is m2 method");
+	}
+
+}
