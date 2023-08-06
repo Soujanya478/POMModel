d7722bf9ac0e334ccb9174a2abe69ab3baaf922c
Amar Prattipati <75680457+amarprathipati@users.noreply.github.com>
2023-08-06 16:35:52 +0530

    added changes in testing.java file

--- src/main/java/com/qa/opencart/pages/testing.java
+++ src/main/java/com/qa/opencart/pages/testing.java
@@ -1,9 +1,9 @@
-package com.qa.opencart.pages.testing
+package com.qa.opencart.pages;
 
 class testing {
 
-Public Static void main(String args[]){
+public static void main(String args[]){
 
-System.out.println("Testing class")
+System.out.println("Testing class");
 }
 }
